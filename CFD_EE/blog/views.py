c6429c83479509db926414999265d438012309eb
Susham Nandi <sushamnandi2468@gmail.com>
2020-10-30 11:28:33 +0530

    additional file changes for heroku

--- CFD_EE/blog/views.py
+++ CFD_EE/blog/views.py
@@ -13,7 +13,8 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
-    ordering = ['date_posted']
+    ordering = ['-date_posted']
+    paginate_by = 5
 
 class PostDetailView(DetailView):
     model = Post
